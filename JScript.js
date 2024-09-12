860c3886475e1e04a79ffdc04f19bda676feeda9
R-S-I-Lab <raileansergiu95@yahoo.com>
2024-09-12 18:38:45 +0300

    JScript.js

--- JScript.js
+++ JScript.js
@@ -1,17 +1,17 @@
-let noClicks = 0
+let noClicks = 0;
 
 function changeColor() {
-    ++noClicks
-    if(noClicks === 1) {
-        document.getElementById("buttonColor").innerHTML = "READY"
-        document.getElementById("buttonColor").className = "btn btn-warning"
+    ++noClicks;
+    if (noClicks === 1) {
+        document.getElementById("buttonColor").innerHTML = "READY";
+        document.getElementById("buttonColor").className = "btn btn-warning";
     } else if (noClicks === 2) {
-        document.getElementById("buttonColor").innerHTML = "GO"
-        document.getElementById("buttonColor").className = "btn btn-success"
+        document.getElementById("buttonColor").innerHTML = "GO";
+        document.getElementById("buttonColor").className = "btn btn-success";
     } else {
-        document.getElementById("buttonColor").innerHTML = "STOP"
-        document.getElementById("buttonColor").className = "btn btn-danger"
+        document.getElementById("buttonColor").innerHTML = "STOP";
+        document.getElementById("buttonColor").className = "btn btn-danger";
         noClicks = 0;
     }
 }
-setInterval(changeColor, 10000)
+setInterval(changeColor, 10000);
